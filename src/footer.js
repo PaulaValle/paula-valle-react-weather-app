4c8de8e81dc88e43959b5a487b5623add0a08877
PaulaValle <paula.valle1.romero@gmail.com>
2022-03-02 12:15:41 +0100

    uploaded my weather app from codesandbox

---
+++ src/footer.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+export default function footer() {
+  return (
+    <footer>
+      <a
+        href="https://github.com/PaulaValle/paulavalleweatherappshecodes"
+        target="_blank"
+      >
+        Open source
+      </a>
+      Coded by
+      <a
+        href="https://es.linkedin.com/in/paula-valle-romero-a8869a184"
+        target="_blank"
+      >
+        Paula Valle
+      </a>
+    </footer>
+  );
+}
---
+++ src/head.js
@@ -0,0 +1,18 @@
+export default function head() {
+  return (
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Paula Valle _ Weather App</title>
+      <link rel="stylesheet" href="src/weatherappstyle.css" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=PT+Mono&display=swap"
+        rel="stylesheet"
+      />
+      <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    </head>
+  );
+}
