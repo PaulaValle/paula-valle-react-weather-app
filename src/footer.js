79277737d21bbc2a9ce65ff65fd06b7657fc1b05
PaulaValle <paula.valle1.romero@gmail.com>
2022-03-02 12:52:09 +0100

    fixed the bugs

--- src/footer.js
+++ src/footer.js
@@ -1,4 +1,5 @@
 import "./styles.css";
+import React from "react";
 
 export default function footer() {
   return (
@@ -6,6 +7,7 @@ export default function footer() {
       <a
         href="https://github.com/PaulaValle/paulavalleweatherappshecodes"
         target="_blank"
+        rel="noopener noreferrer"
       >
         Open source
       </a>
@@ -13,6 +15,7 @@ export default function footer() {
       <a
         href="https://es.linkedin.com/in/paula-valle-romero-a8869a184"
         target="_blank"
+        rel="noopener noreferrer"
       >
         Paula Valle
       </a>
